eb9da993d6f6fa0bf7d192819f49ec116b018175
Daniel020523 <d89stk8g6p@privaterelay.appleid.com>
2026-08-18 18:37:07 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 11474956
+    "zoneId": 11606783
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
